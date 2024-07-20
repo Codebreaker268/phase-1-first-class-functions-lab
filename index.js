b6b9d596965245ce0471451f2a9fe80ec2ecedc1
Godfrey Onyango <godiodhis268@gmail.com>
2024-07-20 13:04:49 +0300

    last

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers=function(scuberDrivers){
+    scuberDrivers=['Antonia', 'Nuru', 'Amari', 'Mo'];
+    return scuberDrivers.slice(0,2);
+};
+const returnLastTwoDrivers=function(scuberDrivers){
+    scuberDrivers=['Antonia', 'Nuru', 'Amari', 'Mo'];
+    return scuberDrivers.slice(2);
+};
+let selectingDrivers=[returnFirstTwoDrivers,returnLastTwoDrivers];
+createFareMultiplier=(integer)=>{
+    return function(){
+        return integer**2;
+    };
+};
+const fareDoubler=(fare)=>{
+    return fare*2;
+};
+const fareTripler=(fare)=>{
+    return fare*3;
+};
+function selectDifferentDrivers(arrayOfDrivers, returnLastTwoDrivers){
+     arrayOfDrivers=['Antonia', 'Nuru', 'Amari', 'Mo'];
+     return returnLastTwoDrivers(arrayOfDrivers);
+     
+     
+};
